11dc0977cbac3f3df9d64fe457ef31d62e5f5fce
LimYunhwi <88yhtserof@naver.com>
2021-10-14 21:35:58 +0900

    Part1 19~24
    1. assert와 guard
    2. 프로토콜
    3. 익스텐션
    4. 열거형
    5. 옵셔널 체이닝
    6. try-catch

--- Part1.playground/Contents.swift
+++ Part1.playground/Contents.swift
@@ -675,3 +675,317 @@ for item in library {
         print("Song: \(song.name), by \(song.artist)")
     }
 }
+
+
+
+//19. assert와 guard
+/*
+ assert
+ - 특정 조건을 체크하고, 조건이 성립되지 않으면 에러와 함께 메세지를 출력하게 할 수 있는 함수
+ -assert함수는 디버깅 모드에서만 동작하고 주로 디버깅 중 조건의 검증을 위하여 사용한다.
+ guard문
+ -뭔가를 검사하여 그 다음에 오는 코드를 실행할지 말지 결정하는 것
+ -guard문에 주어진 조건문이 거짓일 때 구문이 실행된다.
+ 주로 얼리엣이라 하여 조건이 맞지 않으면 이후 문장을 실행하지 않도록 방어코드로 사용한다.
+ */
+
+//assert
+var value2 = 0
+assert(value2 == 0)
+
+//value2 = 2
+//assert(value == 0, "값이 0이 아닙니다.") //조건에 맞지 않으면 에러와 함께 메세지 출력
+
+
+//guard문 - 보통 잘못된 값이 함수에 들어가는 것을 방어하기 위해 사용
+//함수 내부에서 사용하도록 만들어진 구문
+/*
+ guard 조건 else {
+ //조건이 false이면 else 구문이 실행되고 return or throw or break을 통해 이 후 코드를 실행하지 않도록 한다.
+ }
+ */
+func guardTest(value: Int) {
+    guard value == 0 else {
+        return
+    }
+    //value가 0일 때만 print문 출력
+    print("안녕하세요. guardTest입니다.")
+}
+
+guardTest(value: 2)//value가 0이 아니기 떄문에 출력 안 됨
+guardTest(value: 0)
+
+//guard문을 이용해 옵셔널 바인딩을 할 수 있다.
+//guard문을 사용하면 옵셔널바인딩된 상수를 조건문 밖에서도 사용할 수 있다.
+func guardOptionalBinding(value: Int?) {
+    guard let value = value  else {// value인수에 값이 있다면 value상수에 할당되도록한다. value인수가 nil이라면 else구문 실행
+        return
+    }
+    //value가 0일 때만 print문 출력
+    print("안녕하세요. \(value)입니다.")
+}
+
+guardOptionalBinding(value: 2)
+guardOptionalBinding(value: nil)
+
+
+//20. 프로토콜
+/*
+ 특정 역할을 하기 위한 메서드, 프로퍼티, 기타 요구하상 등의 청사진
+ 구조체 클래스 열거형은 프로토콜을 채택하여 프로토콜이 요구하는 사항을 구현할 수 있다.
+ 프로토콜의 요구사항을 모두 충족하면 해당 타입이 채택한 프로토콜을 준수한다고 말할 수 있다.
+ */
+
+protocol SomeProtocol {
+    
+}
+protocol SomeProtocol2 {
+    
+}
+
+struct SoneStructure: SomeProtocol, SomeProtocol2 { //프로토콜 채택하기
+    
+}
+/*
+class SomeClass : 부모클래스, 프로토콜1, 프로토콜2 {
+}
+
+ 프로토콜은 특정 역할을 수행하는 메서드, 프로퍼티, 기타 요구사항 등에 청사진 요구, 채택한 타입에게 요구사항을 준수하라고 요청
+ */
+protocol FirstProtocol {
+    //프로퍼티의 요구사항으로 이름과 타입, get set을 설정할 수 있다.
+    var name: Int {get set}
+    var age:Int {get}
+}
+
+protocol AnotherProtocol {
+    //타입 프로퍼티를 요구하려면 static 키워드 필수
+    static var someTypeProperty: Int {get set}
+}
+
+protocol FullyNames {
+    var fullName: String {get set}
+    func printFullName(name: String)
+}
+
+struct Person: FullyNames {
+    var fullName: String
+    
+    func printFullName(name: String) {
+        print(fullName)
+    }
+}
+
+protocol SomeProtocol3 {
+    //프로토콜이 요구사항으로 메서드 생성 시 메서드의 매개변수를 설정해주어야 하는데, default 값은 지정할 수 없다.
+    func someTypeMethode()
+}
+
+//이니셜 라이저 요구사항
+//프로토콜은 생성자도 요구할 수 있다.
+protocol SomeProtocol4 {
+    init(someParameter: Int)
+}
+
+protocol SomeProtocol5 {
+    init()
+}
+
+class SomeClass2: SomeProtocol5 {
+    // 클래스에서 프로토콜이 요구한 생성자를 구현하려면 required 키워드가 필요하다.
+    //만약 클래스 자체가 상속받을 수 없는 final 클래스라면 required 키워드가 필요없다.
+    //구조체에서 생성자 요구사항을 준수할 때는 required 키워드가 필요없다.
+    //즉 상속 가능 여부에 따라 required 필요 여부가 달라진다.
+    required init() {
+        
+    }
+}
+
+//21. 익스텐션 extension
+/*
+ 스위프트의 강력한 기능
+ 기존의 클래스, 구조체, 열거형, 프로토콜에 새로운 기능을 추가하는 기능
+ 새로운 기능을 추가할 수는 있지만, 기존의 기능을 오버라이드(재정의)할 수 없다.
+ 
+ 익스텐션이 타입에 추가할 수 있는 기능
+ - 연산 타입 프로퍼티/ 연산 인스턴스 프로퍼티
+ - 타입 메서드/ 인스턴스 메서드
+ -이니셜라이저
+ -서브스크립트
+ -중첩타입
+ -특정 프로토콜을 준수할 수 있도록 기능 추가
+ */
+
+//Int형 타입의 연산 프로퍼티를 추가해 기능 확장
+//extension은 연산 프로퍼티는 추가할 수 있지만, 저장프로퍼티는 추가할 수 없다.
+//타입에 정의되어있는 기존의 프로퍼티의 프로퍼티 감시자를 추가할 수 없다.
+extension Int {
+    var isEven: Bool{
+        return self % 2 == 0
+    }
+    var isOdd: Bool {
+        return self % 2 == 1
+    }
+}
+
+var number2 = 3
+number2.isOdd
+number2.isEven
+
+
+//extensin을 통해 타입에 메서드 추가하기
+extension String {
+    func convertToInt() -> Int? {
+        return Int(self)
+    }
+}
+
+var string2 = "0"
+string2.convertToInt()
+
+
+
+//22. 열거형 enumeration
+/*
+ 연관성이 있는 값을 모아놓은 것을 말한다.
+ 열거형의 각 항목은 그 자체로 하나의 값이다.
+ 자체로도 하나의 값이지만 원시값을 가질 수 있다.
+ 열거형은 연관값도 가질 수 있다. - 열거형 내 항목이 자신과 연관된 값을 가질 수 있다.
+ */
+
+//열거형은 하나의 타입으로 사용할 수 있다.
+enum CompassPoint: String {
+    case north = "북"
+    case south = "남"
+    case east = "동"
+    case west = "서"
+}
+
+var direction = CompassPoint.east
+direction = .west //direction 변수가 CompassPoint로 타입추론되었기 때문에 이러한 방식으로 사용 가능
+
+//열거형은 switch 구문과 사용하면 다양하게 활용할 수 있다.
+switch direction {
+case .north:
+    print(direction.rawValue)
+case .south:
+    print(direction.rawValue)
+case .east:
+    print(direction.rawValue)
+case .west:
+    print(direction.rawValue)
+}
+
+//원시값을 가지고 열거형을 반환하게 할 수도 있다.
+//열거형 인스턴스를 생성할 때 매개변수로 원시값을 넘겨주면 된다.
+ let direction2 = CompassPoint(rawValue: "남") //direction2의 열거형 항목으로 "남" 인수값과 원시값이 대응하는 south 항목이 할당된다.
+
+
+//열거형의 연관값
+enum PhoneError {
+    case unknown
+    case batteryLow(String)
+}
+
+let error = PhoneError.batteryLow("배터리가 곧 방전됩니다.")
+//연관값을 추출하려면 if case..? 또는 switch문 사용
+switch error {
+case .batteryLow(let message): //연관값을 넘겨받을 수 있는 상수 선언
+    print(message)
+case .unknown:
+    print("알 수 없는 에러입니다.")
+}
+
+
+//23. 옵셔널 체이닝
+/*
+ 옵셔널에 속해 nil일지도 모르는 프로퍼티, 메서드, 서브스크립션 등을 가져오거나 호출할 때 사용할 수 있는 일련의 과정
+  옵셔널에 값이 있다면 프로퍼티, 메서드 등을 호출하고, 옵셔널이 nil이라면 nil을 반환한다.
+ */
+
+struct Developer {
+    var name: String
+}
+
+struct Company {
+    let name: String
+    var developer: Developer?
+}
+
+var developer = Developer(name: "han")
+var company = Company(name: "Gunter", developer: developer)
+print(company.developer)
+print(company.developer?.name) //?로 옵셔널 체이닝 - Optional로 값이 감싸진 채로 있다 -> 옵셔널 바인딩을 통해 값 추출 가능
+print(company.developer!.name) //!로 옵셔널 체이닝 - 강제 추출된다
+
+//이렇게 내부 프로퍼티를 접근할 때, 옵셔널 바인딩 없이 옵셔널 체이닝을 사용하여 쉽게 값에 접근할 수 있다.
+
+
+
+//24. try-catch
+/*
+ 에러 처리란?
+ 프로그램 내에서 에러가 발생한 상황에 대응하고 이를 복구하는 과정
+ 스위프트에서 런타임 중 에러가 발생한 경우 이를 처리하기 위해
+ 발생(throwing), 감지(catching), 전파(propagating), 조작(manipulating) 을 지원하는 일급(?) 클래스를 제공한다.
+ 
+ 오류 처리 기능을 통해 이러 상황들을 구별하여 프로그램 자체적으로 오류를 해결할 수도 있고, 사용자에게 어떤 오류가 발생했는지 알려줄 수도 있다.
+ */
+// 스위프트에서 에러는 Error 프로토콜을 따르는 타입의 값으로 표현된다.
+//Error 프로토콜은 요구사항이 없는 빈 프로토콜이지만 에러를 표현하기 위해서 채택해야한다.
+//스위프트의 열거형은 오류 원인을 나누고, 해당 오류에 대한 특성에 대한 추가 정보를 전달하는모델을 만드는데에 적합하다
+
+enum PhoneError2: Error {
+    case unkown
+    case batteryLow(batteryLevel: Int)
+}
+
+//throw PhoneError2.batteryLow(batteryLevel: 20) //throw 구문을 통해서 에러가 발생할 것 같은 부분에 에러를 던져준다.
+
+//던져진 오류를 처리하기 위한 코드를 작성해야한다. 던져진 오류가 무엇인지 판단하여 다른 방법으로 문제를 해야해 본다던지, 사용자에게 오류를 알리는 등의 코드를 작성해야한다.
+
+/*
+ 스위프트에서 오류를 처리하는 4가지 방법
+ 1. 함수에서 발생한 오류를 해당 함수를 호출한 코드에 전파
+ 2. do-catch구문을 이용해 오류 처리
+ 3. 옵셔널 값으로 오류 처리
+ 4. 오류가 발생하지 않을 것이라 확신하기
+ */
+
+//1. 함수에서 발생한 오류를 해당 함수를 호출한 코드에 전파
+func checkPhoneBatteryStatus(batteryLevel: Int) throws -> String {
+    //false일 경우 else 구문 실행, 함수 조기 종료
+    guard batteryLevel != -1 else {throw PhoneError2.unkown}
+    guard batteryLevel > 20 else {throw PhoneError2.batteryLow(batteryLevel: 20)}
+    
+    //두 guard문이 true여야 아래 문장 실행된다.
+    return "배터리 상태가 양호합니다."
+}
+
+//이 메서드를 사용하려면 오류를 발생할 수도 있기 때문에 do-catch 구문이나 try?, try!를 사용하여 오류를 처리할 수 있어야 한다.
+
+//(1) do-catch 구문
+do {
+    try checkPhoneBatteryStatus(batteryLevel: 20)
+} catch PhoneError2.unkown {
+    print("알 수 없는 에러")
+} catch PhoneError2.batteryLow(let baterryLevel) {//연관값을 상수로 전달되게 한다.
+    print("배터리 부족: \(baterryLevel)%")
+} catch { //catch 뒤에 오류의 종류를 명시하지 않아도 코드블록을 생성하며 블록 내부에 암시적으로 error라는 이름의 지역상수가 오류 내용을 담아 들어오게 된다.
+    print("그 외 오류 발생: \(error)")
+}
+
+//(2) try?
+//오류를 옵셔널 값으로 변환하여 처리할 수 있다.
+//try ? 구문을 통해 동작하던 코드가 오류를 던지면 그 코드의 반환값은 nil이 된다.
+//만약 오류가 나지 않으면 Optional 값이 반환된다.
+let status1 = try? checkPhoneBatteryStatus(batteryLevel: 30)
+print(status1)
+let status2 = try? checkPhoneBatteryStatus(batteryLevel: 20)
+print(status2)
+
+
+//(3) try!
+//에러를 던져주지 않은 것이라 확신할 때 사용
+let status3 = try! checkPhoneBatteryStatus(batteryLevel: 30)
+print(status3)
