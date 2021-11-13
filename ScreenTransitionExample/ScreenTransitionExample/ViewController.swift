283d2c77ac0fb2adda59142759896ca85a369b70
LimYunhwi <88yhtserof@naver.com>
2021-11-14 00:20:51 +0900

    Study Puch to Segue

--- ScreenTransitionExample/ScreenTransitionExample/ViewController.swift
+++ ScreenTransitionExample/ScreenTransitionExample/ViewController.swift
@@ -8,7 +8,8 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+//Segue로 Present -> Segue객체와 Present 방식(화면 위에 화면 덮기/ Present Modally 사용)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
